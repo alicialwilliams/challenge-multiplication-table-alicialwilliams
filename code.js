9d6a95da75de7275960aabc14395c23272d32cf4
alicialwilliams <63070379+alicialwilliams@users.noreply.github.com>
2021-10-12 01:57:50 -0400

    set the output data into table rows and columns with nested for loops.

--- code.js
+++ code.js
@@ -1,1 +1,22 @@
-// Your code here
+const table = document.getElementById('mult-table');
+let output = '';
+
+for (let i = 1; i <= 12; i++) {
+    output += '<tr>';
+    for (let j = 1; j <= 12; j++) {
+        if (i === 1 && j === 1)
+            output += '<th>&times</th>'//x factor
+        else {
+            if (i === 1 || j === 1)
+                output += '<th>' + i * j + '</th>'
+            else {
+                output += '<td>' + i * j + '</td>'
+            }
+        }
+
+    }
+    output += '</tr>'
+
+}
+
+table.innerHTML = output;
