f14d22537a3320aa4ed9a122395db21e8b75cc9e
alicialwilliams <63070379+alicialwilliams@users.noreply.github.com>
2021-10-14 01:11:10 -0400

    help from study hall coach, Kyle Thomas, with the table headerconditional statements for the zero row/columns.

--- code.js
+++ code.js
@@ -1,22 +1,21 @@
 const table = document.getElementById('mult-table');
+
 let output = '';
-
-for (let i = 0; i <= 10; i++) {
+for (let i = 0; i <= 11; i++) {
     output += '<tr>';
-    for (let j = 0; j <= 10; j++) {
-        if (i === 0 && j === 0)
-            output += '<th>&times</th>'//x factor
-        else {
-            if (i === 0 || j === 0)
-                output += '<th>' + i * j + '</th>'
-            else {
-                output += '<td>' + i * j + '</td>'
-            }
+    for (let j = 0; j <= 11; j++) {
+        if (i == 0 && j == 0)
+            output += '<th>&times</th>'
+        else if (j >= 1 && i == 0) {
+            output += '<th>' + (j - 1) + '</th>'
+        } else if (i >= 1 && j == 0) {
+            output += '<th>' + (i - 1) + '</th>'
+        } else {
+            output += '<td>' + (i - 1) * (j - 1) + '</td>'
         }
-
     }
     output += '</tr>'
-
 }
 
 table.innerHTML = output;
+
